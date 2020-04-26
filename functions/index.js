dfa15ec86530059705886ab85136b8d2bbcc79b7
roll1226 <tomo_yasu_4724@icloud.com>
2020-04-26 18:05:59 +0900

    [ADD]express to functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions')
 const { Nuxt } = require('nuxt')
+const express = require('express')
+const app = express()
+
 const nuxt = new Nuxt({
   dev: false,
   buildDir: '.nuxt'
@@ -22,7 +25,18 @@ const nuxt = new Nuxt({
 
 // app.use(handler)
 // exports.app = functions.https.onRequest(app)
-exports.app = functions.https.onRequest(async (req, res) => {
-  await nuxt.ready()
-  return nuxt.render(req, res)
-})
+// exports.app = functions.https.onRequest(async (req, res) => {
+//   await nuxt.ready()
+//   return nuxt.render(req, res)
+// })
+function handleRequest(req, res) {
+  res.set('Cache-Control', 'public, max-age=10, s-maxage=10')
+  return new Promise((resolve, reject) => {
+    nuxt.render(req, res, (promise) => {
+      promise.then(resolve).catch(reject)
+    })
+  })
+}
+
+app.use(handleRequest)
+exports.ssr = functions.https.onRequest(app)
