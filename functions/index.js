238ec31b564f37f927a296e7486c8e6006f3f3a4
roll1226 <tomo_yasu_4724@icloud.com>
2020-04-27 19:01:27 +0900

    [FIX]Reload

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions')
 const { Nuxt } = require('nuxt')
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
+const envs = functions.config().environment
 
 const nuxt = new Nuxt({
   dev: false,
@@ -11,32 +12,23 @@ const nuxt = new Nuxt({
   // }
 })
 
-// function handler(req, res) {
-//   res.set('Cache-Control', 'public, max-age=600, s-maxage=1200')
-//   nuxt
-//     .renderRoute('/', { req })
-//     .then((result) => {
-//       res.send(result.html)
+Object.entries(envs).forEach((k, v) => {
+  process.env[`${k}`.toUpperCase()] = v
+})
+
+exports.app = functions.https.onRequest(async (req, res) => {
+  await nuxt.ready()
+  return nuxt.render(req, res)
+})
+
+// function handleRequest(req, res) {
+//   res.set('Cache-Control', 'public, max-age=10, s-maxage=10')
+//   return new Promise((resolve, reject) => {
+//     nuxt.render(req, res, (promise) => {
+//       promise.then(resolve).catch(reject)
 //     })
-//     .catch((e) => {
-//       res.send(e)
-//     })
+//   })
 // }
 
-// app.use(handler)
-// exports.app = functions.https.onRequest(app)
-// exports.app = functions.https.onRequest(async (req, res) => {
-//   await nuxt.ready()
-//   return nuxt.render(req, res)
-// })
-function handleRequest(req, res) {
-  res.set('Cache-Control', 'public, max-age=10, s-maxage=10')
-  return new Promise((resolve, reject) => {
-    nuxt.render(req, res, (promise) => {
-      promise.then(resolve).catch(reject)
-    })
-  })
-}
-
-app.use(handleRequest)
-exports.ssr = functions.https.onRequest(app)
+// app.use(handleRequest)
+// exports.ssr = functions.https.onRequest(app)
