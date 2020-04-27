c8ed9e12bf6607f077f41793fb787046d1a74925
roll1226 <tomo_yasu_4724@icloud.com>
2020-04-27 19:02:05 +0900

    [FIX]Reload

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: '@/plugins/firebase', mode: 'server' }],
   /*
    ** Nuxt.js dev-modules
    */
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -6,11 +6,13 @@ import 'firebase/storage'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID
   })
 
   firebase.firestore()
